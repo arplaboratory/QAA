67e7a09b13465a83998f3ae8651b082efc90e83f
Jiuhong Xiao <jx1190@log-1.hpc.nyu.edu>
2024-07-25 21:33:14 -0400

    fix

--- dataloaders/GenericDataloader.py
+++ dataloaders/GenericDataloader.py
@@ -46,7 +46,7 @@ class GenericDataModule(pl.LightningDataModule):
         self.val_datasets_cfg = load_datasets_config(self.val_dataset_names)
         self.test_datasets_cfg = load_datasets_config(self.test_dataset_names)
         self.train_cfg_training = train_cfg_training
-        if self.train_cfg_training.recompute_interval != 0:
+        if self.train_cfg_training.recompute_clusters and self.train_cfg_training.recompute_interval!=0
             self.recompute_count = 0
         self.model = None
 
@@ -190,12 +190,14 @@ class GenericDataModule(pl.LightningDataModule):
                                                         random_sample_from_each_place=GSV_params.training.random_sample_from_each_place,
                                                         transform=self.train_transform)
         else:
-            if self.train_cfg_training.recompute_interval!=0 and recompute_count == self.train_cfg_training.recompute_interval:
+            if self.train_cfg_training.recompute_clusters and self.train_cfg_training.recompute_interval!=0 and self.recompute_count == self.train_cfg_training.recompute_interval:
+                print("RECOMPUTE")
                 self.train_datasets[index].reload(model=self.model, recompute=True)
                 self.recompute_count = 0
             else:
+                print("SHUFFLE")
                 self.train_datasets[index].reload(model=self.model, recompute=False)
-                if self.train_cfg_training.recompute_interval!=0:
+                if self.train_cfg_training.recompute_clusters and self.train_cfg_training.recompute_interval!=0
                     self.recompute_count += 1
 
     def train_dataloader(self):
