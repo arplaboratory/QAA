b797927ea6313b71e38588ee67b93cbfd4f916b7
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2024-07-26 13:36:05 -0400

    clean memory

--- vpr_model.py
+++ vpr_model.py
@@ -234,12 +234,16 @@ class VPRModel(pl.LightningModule):
         descriptors = self(places)
         if dataloader_idx is None: # Only one val dataset
             dataloader_idx = 0
-        self.val_outputs[dataloader_idx].append(descriptors.detach().cpu())
+        if self.current_dataloader_idx != dataloader_idx:
+            self.val_calculate_recall(self.current_dataloader_idx)
+            self.current_dataloader_idx = dataloader_idx
+        self.val_outputs.append(descriptors.detach().cpu())
         return descriptors.detach().cpu()
     
     def on_validation_epoch_start(self):
         # reset the outputs list
-        self.val_outputs = [[] for _ in range(len(self.trainer.datamodule.val_datasets))]
+        self.val_outputs = []
+        self.current_dataloader_idx = 0
     
     def on_validation_epoch_end(self):
         """this return descriptors in their order
@@ -247,26 +251,109 @@ class VPRModel(pl.LightningModule):
         for this project (MSLS val, Pittburg val), it is always references then queries
         [R1, R2, ..., Rn, Q1, Q2, ...]
         """
+        # reset the outputs list
+        self.val_outputs = []
+
+        print("Update model for recomputing if recomputing is enabled")
+        self.trainer.datamodule.model = self # Not sure if this is correct
+
+    def val_calculate_recall(self, dataloader_idx):
+        # Clean memory once one dataset finished
         val_step_outputs = self.val_outputs
-
         dm = self.trainer.datamodule
-
-        # The following line is a hack: if we have only one validation set, then
-        # we need to put the outputs in a list (Pytorch Lightning does not do it presently)
-        # if len(dm.val_datasets)==1: # we need to put the outputs in a list
-        #     val_step_outputs = [val_step_outputs]
+        val_dataset = dm.val_datasets[dataloader_idx]
+        val_set_name = val_dataset.dataset_name
+        feats = torch.concat(val_step_outputs, dim=0)
         
-        for i, val_dataset in enumerate(dm.val_datasets):
-            val_set_name = val_dataset.dataset_name
-            feats = torch.concat(val_step_outputs[i], dim=0)
+        if val_set_name == "mapillary_sls":
+            # split to ref and queries
+            num_references = val_dataset.num_references
+            positives = val_dataset.pIdx
+        else:
+            num_references = val_dataset.num_references
+            positives = val_dataset.ground_truth
+
+        r_list = feats[ : num_references]
+        q_list = feats[num_references : ]
+        pitts_dict = utils.get_validation_recalls(
+            r_list=r_list, 
+            q_list=q_list,
+            k_values=[1, 5, 10, 15, 20, 50, 100],
+            gt=positives,
+            print_results=True,
+            dataset_name=val_set_name,
+            faiss_gpu=self.faiss_gpu
+        )
+        del r_list, q_list, feats, num_references, positives
+        self.log(f'{val_set_name}_{val_dataset.split}/R1', pitts_dict[1], prog_bar=False, logger=True)
+        self.log(f'{val_set_name}_{val_dataset.split}/R5', pitts_dict[5], prog_bar=False, logger=True)
+        self.log(f'{val_set_name}_{val_dataset.split}/R10', pitts_dict[10], prog_bar=False, logger=True)        
+        print('\n\n')
+
+        self.val_outputs = []
+
+    # For validation, we will also iterate step by step over the validation set
+    # this is the way Pytorch Lghtning is made. All about modularity, folks.
+    def test_step(self, batch, batch_idx, dataloader_idx=None):
+        places, _ = batch
+        descriptors = self(places)
+        if dataloader_idx is None: # Only one val dataset
+            dataloader_idx = 0
+        if self.current_dataloader_idx != dataloader_idx:
+            self.test_calculate_recall(self.current_dataloader_idx)
+            self.current_dataloader_idx = dataloader_idx
+        self.test_outputs.append(descriptors.detach().cpu())
+        return descriptors.detach().cpu()
+    
+    def on_test_epoch_start(self):
+        # reset the outputs list
+        self.test_outputs = []
+        self.current_dataloader_idx = 0
+    
+    def on_test_epoch_end(self):
+        """this return descriptors in their order
+        depending on how the validation dataset is implemented 
+        for this project (MSLS val, Pittburg val), it is always references then queries
+        [R1, R2, ..., Rn, Q1, Q2, ...]
+        """
+
+        # reset the outputs list
+        self.test_outputs = []
+
+    def test_calculate_recall(self, dataloader_idx):
+        # Clean memory once one dataset finished
+        test_step_outputs = self.test_outputs
+        dm = self.trainer.datamodule
+        test_dataset = dm.test_datasets[dataloader_idx]
+        test_set_name = test_dataset.dataset_name
+        feats = torch.concat(test_step_outputs, dim=0)
             
-            if val_set_name == "mapillary_sls":
-                # split to ref and queries
-                num_references = val_dataset.num_references
-                positives = val_dataset.pIdx
-            else:
-                num_references = val_dataset.num_references
-                positives = val_dataset.ground_truth
+        if test_set_name == "mapillary_sls":
+            # split to ref and queries
+            num_references = test_dataset.num_references
+            positives = None
+            testing = True # This flag is for msls
+
+            r_list = feats[ : num_references]
+            q_list = feats[num_references : ]
+            preds = utils.get_validation_recalls(
+                r_list=r_list, 
+                q_list=q_list,
+                k_values=[1, 5, 10, 15, 20, 50, 100],
+                gt=positives,
+                print_results=True,
+                dataset_name=test_set_name,
+                faiss_gpu=self.faiss_gpu,
+                testing=testing,
+            )
+            del r_list, q_list, feats, num_references, positives
+            assert test_dataset.split == "test"
+            print(f"Save predictions to msls_preds.txt")
+            test_dataset.save_predictions(preds, f'UniVG/{self.logger.version}/checkpoints/msls_preds.txt')
+        else:
+            num_references = test_dataset.num_references
+            positives = test_dataset.ground_truth
+            testing = False # This flag is for other dataset that has ground truth
 
             r_list = feats[ : num_references]
             q_list = feats[num_references : ]
@@ -276,97 +363,14 @@ class VPRModel(pl.LightningModule):
                 k_values=[1, 5, 10, 15, 20, 50, 100],
                 gt=positives,
                 print_results=True,
-                dataset_name=val_set_name,
-                faiss_gpu=self.faiss_gpu
+                dataset_name=test_set_name,
+                faiss_gpu=self.faiss_gpu,
+                testing=testing,
             )
             del r_list, q_list, feats, num_references, positives
-            self.log(f'{val_set_name}_{val_dataset.split}/R1', pitts_dict[1], prog_bar=False, logger=True)
-            self.log(f'{val_set_name}_{val_dataset.split}/R5', pitts_dict[5], prog_bar=False, logger=True)
-            self.log(f'{val_set_name}_{val_dataset.split}/R10', pitts_dict[10], prog_bar=False, logger=True)        
+            self.log(f'{test_set_name}_{test_dataset.split}/R1', pitts_dict[1], prog_bar=False, logger=True)
+            self.log(f'{test_set_name}_{test_dataset.split}/R5', pitts_dict[5], prog_bar=False, logger=True)
+            self.log(f'{test_set_name}_{test_dataset.split}/R10', pitts_dict[10], prog_bar=False, logger=True)        
         print('\n\n')
 
-        # reset the outputs list
-        self.val_outputs = []
-
-        print("Update model for recomputing if recomputing is enabled")
-        dm.model = self # Not sure if this is correct
-
-    # For validation, we will also iterate step by step over the validation set
-    # this is the way Pytorch Lghtning is made. All about modularity, folks.
-    def test_step(self, batch, batch_idx, dataloader_idx=None):
-        places, _ = batch
-        descriptors = self(places)
-        if dataloader_idx is None: # Only one val dataset
-            dataloader_idx = 0
-        self.test_outputs[dataloader_idx].append(descriptors.detach().cpu())
-        return descriptors.detach().cpu()
-    
-    def on_test_epoch_start(self):
-        # reset the outputs list
-        self.test_outputs = [[] for _ in range(len(self.trainer.datamodule.test_datasets))]
-    
-    def on_test_epoch_end(self):
-        """this return descriptors in their order
-        depending on how the validation dataset is implemented 
-        for this project (MSLS val, Pittburg val), it is always references then queries
-        [R1, R2, ..., Rn, Q1, Q2, ...]
-        """
-        test_step_outputs = self.test_outputs
-
-        dm = self.trainer.datamodule
-        # The following line is a hack: if we have only one validation set, then
-        # we need to put the outputs in a list (Pytorch Lightning does not do it presently)
-        # if len(dm.val_datasets)==1: # we need to put the outputs in a list
-        #     val_step_outputs = [val_step_outputs]
-        
-        for i, test_dataset in enumerate(dm.test_datasets):
-            test_set_name = test_dataset.dataset_name
-            feats = torch.concat(test_step_outputs[i], dim=0)
-            
-            if test_set_name == "mapillary_sls":
-                # split to ref and queries
-                num_references = test_dataset.num_references
-                positives = None
-                testing = True # This flag is for msls
-
-                r_list = feats[ : num_references]
-                q_list = feats[num_references : ]
-                preds = utils.get_validation_recalls(
-                    r_list=r_list, 
-                    q_list=q_list,
-                    k_values=[1, 5, 10, 15, 20, 50, 100],
-                    gt=positives,
-                    print_results=True,
-                    dataset_name=test_set_name,
-                    faiss_gpu=self.faiss_gpu,
-                    testing=testing,
-                )
-                del r_list, q_list, feats, num_references, positives
-                assert test_dataset.split == "test"
-                print(f"Save predictions to msls_preds.txt")
-                test_dataset.save_predictions(preds, f'UniVG/{self.logger.version}/checkpoints/msls_preds.txt')
-            else:
-                num_references = test_dataset.num_references
-                positives = test_dataset.ground_truth
-                testing = False # This flag is for other dataset that has ground truth
-
-                r_list = feats[ : num_references]
-                q_list = feats[num_references : ]
-                pitts_dict = utils.get_validation_recalls(
-                    r_list=r_list, 
-                    q_list=q_list,
-                    k_values=[1, 5, 10, 15, 20, 50, 100],
-                    gt=positives,
-                    print_results=True,
-                    dataset_name=test_set_name,
-                    faiss_gpu=self.faiss_gpu,
-                    testing=testing,
-                )
-                del r_list, q_list, feats, num_references, positives
-                self.log(f'{test_set_name}_{test_dataset.split}/R1', pitts_dict[1], prog_bar=False, logger=True)
-                self.log(f'{test_set_name}_{test_dataset.split}/R5', pitts_dict[5], prog_bar=False, logger=True)
-                self.log(f'{test_set_name}_{test_dataset.split}/R10', pitts_dict[10], prog_bar=False, logger=True)        
-        print('\n\n')
-
-        # reset the outputs list
         self.test_outputs = []
