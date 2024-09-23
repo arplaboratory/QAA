a1fbe332381b441c3edaa0bf441f4459f845878d
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2024-09-22 22:42:09 -0400

    fix

--- dataloaders/CliqueGenericDataset.py
+++ dataloaders/CliqueGenericDataset.py
@@ -309,7 +309,7 @@ class CliqueGenericDataset(Dataset):
     @staticmethod
     def image_loader(path):
         try:
-            return torchvision.io.read_image(path).convert('RGB')
+            return torchvision.io.read_image(path, mode=torchvision.io.image.ImageReadMode.RGB)
         except UnidentifiedImageError:
             print(f'Image {path} could not be loaded')
             return Image.new('RGB', (224, 224))
