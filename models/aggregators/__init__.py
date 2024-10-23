ee76d8465fbe172a4283402d6698643ccf827def
Jiuhong Xiao <jx1190@log-3.hpc.nyu.edu>
2024-10-22 22:59:26 -0400

    conditioned

--- models/aggregators/__init__.py
+++ models/aggregators/__init__.py
@@ -8,3 +8,4 @@ from .domain_queries_salad_shared import DomainQueriesSALADShared
 from .domain_queries_salad_score import DomainQueriesSALADScore
 from .domain_queries_salad_feature import DomainQueriesSALADFeature
 from .domain_queries_salad_sf import DomainQueriesSALADSF
+from .conditioned_queries_salad import ConditionedQueriesSALAD
