2a92776962a018c6cbf605a7a1b253f49b4ebaad
Jiuhong Xiao <jx1190@log-1.hpc.nyu.edu>
2024-10-29 14:37:48 -0400

    ?

--- models/aggregators/domain_queries_salad_feature_back_dc.py
+++ models/aggregators/domain_queries_salad_feature_back_dc.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from .attention import QuerySelfAttn, QueryCrossAttn
 from .salad import log_optimal_transport
 
+
 class DomainQueriesSALADFeatureBackDC(nn.Module):
     """
     This class represents the Sinkhorn Algorithm for Locally Aggregated Descriptors (SALAD) model.
