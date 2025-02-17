309d198fbacad80f036984b3c51fc0fb3f2808f8
Jiuhong Xiao <jx1190@nyu.edu>
2025-02-16 20:12:43 -0500

    update measure flop

--- utils/measure_flop.py
+++ utils/measure_flop.py
@@ -10,6 +10,10 @@ def measure_flop(model):
     fwd_flops = measure_flops(model, model_fwd)/ 1e9
     model_backbone_fwd = lambda: model.backbone(x)
     backbone_flops = measure_flops(model, model_backbone_fwd)/ 1e9
+    model.logger.experiment.log({"fwd_flops": fwd_flops,
+                                 "backbone_flops": backbone_flops,
+                                 "agg_flops": fwd_flops-backbone_flops,
+                                 "model_output_dim": model(x).shape[1]})
     print(f"Model: {fwd_flops} GFLOPS, Backbone: {backbone_flops}, Agg: {fwd_flops-backbone_flops}")
     print(f"Model dim: {model(x).shape}")
     if model.agg_arch == "QAA":
