5e2f11a673ada1b895d877eaaf142e94436828c3
Jiuhong Xiao <jx1190@nyu.edu>
2025-03-04 02:14:50 -0500

    update config

--- models/helper.py
+++ models/helper.py
@@ -75,7 +75,6 @@ def get_aggregator(agg_arch='ConvAP', agg_config={}):
         assert 'cluster_dim' in agg_config
         assert 'token_dim' in agg_config
         assert 'num_queries' in agg_config
-        assert agg_config['num_queries'] >= agg_config['num_clusters'], 'Number of queries must be greater than or equal to number of clusters'
         return aggregators.QAA(**agg_config)
 
     elif 'salad' == agg_arch.lower():
